2e3b2de8c231d58c1fcdfa1b951fef0c00093dd4
Jesse Klijn <jesse.klijn@hva.nl>
2016-11-16 11:47:24 +0100

    Gitignorefix

--- D1_GameJam03/GameJam03/Assets/Scripts/EnemyController.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-public class EnemyController : MonoBehaviour {
-
-    public List<GameObject> Enemies;
-}
--- D1_GameJam03/GameJam03/Assets/Scripts/ShopHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class ShopHandler : MonoBehaviour {
-
-    public bool isOpen;
-}
